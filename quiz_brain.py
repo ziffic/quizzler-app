8eec5453a2197b3a9d05198e0a608397fa6e6a71
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-27 16:20:11 -0700

    finished app

--- quiz_brain.py
+++ quiz_brain.py
@@ -24,15 +24,8 @@ class QuizBrain:
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
-
-    def true_pressed(self):
-        self.check_answer("true")
-
-    def false_pressed(self):
-        self.check_answer("false")
+            return False
+        # print(f"Your current score is: {self.score}/{self.question_number}")
+        # print("\n")
